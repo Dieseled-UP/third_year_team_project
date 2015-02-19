e61887926840b348588e9e4c92f46f76e53f3774
denis bourne <bourne.denis@gmail.com>
2015-02-19 21:52:45 +0000

    added to login page

--- team_nerd_bank/src/gui/Login.java
+++ team_nerd_bank/src/gui/Login.java
@@ -1,18 +1,28 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 
 public class Login extends JFrame implements Runnable {
-	public Login() {
-	}
 
 	private static final long serialVersionUID = 1L;
+	
+	public Login() {
+		
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("NERD BANK");
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 31));
+		lblNewLabel.setBounds(219, 12, 214, 49);
+		getContentPane().add(lblNewLabel);
+		
+	}
 
 	@Override
 	public void run() {
 
 		
 	}
-
 }
