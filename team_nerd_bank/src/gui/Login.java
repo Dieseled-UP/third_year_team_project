a45216148b08eef3bc063edd6eff34f8b587651f
denis bourne <bourne.denis@gmail.com>
2015-02-19 21:34:42 +0000

    first commit

---
+++ team_nerd_bank/src/gui/Login.java
@@ -0,0 +1,18 @@
+package gui;
+
+import javax.swing.JFrame;
+
+
+public class Login extends JFrame implements Runnable {
+	public Login() {
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void run() {
+
+		
+	}
+
+}
