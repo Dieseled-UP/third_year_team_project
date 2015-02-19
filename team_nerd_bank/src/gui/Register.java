8d5114bb900040b52d48e7cc4d96d310c0acdf25
denis bourne <bourne.denis@gmail.com>
2015-02-19 21:37:42 +0000

    added register class

---
+++ team_nerd_bank/src/gui/Register.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.JFrame;
+
+
+public class Register extends JFrame implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void run() {
+
+		
+	}
+
+}
