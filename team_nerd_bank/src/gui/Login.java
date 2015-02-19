fe48486a6d6815c2468242bd2539a4b580123032
denis <bourne.denis@gmail.com>
2015-02-19 22:57:53 +0000

    run method impelmented and image added

--- team_nerd_bank/src/gui/Login.java
+++ team_nerd_bank/src/gui/Login.java
@@ -1,8 +1,16 @@
 package gui;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
 import java.awt.Font;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.JTextField;
 
 
@@ -12,6 +20,10 @@ public class Login extends JFrame implements Runnable {
 	private JTextField txtAutoPin;
 	private JLabel lblAutoPin;
 	private JLabel lblTitle;
+	private JLabel lblimg;
+	private String path = "Assets/banklogo.jpg";
+	private File file;
+	private BufferedImage image;
 	
 	public Login() {
 		
@@ -23,7 +35,8 @@ public class Login extends JFrame implements Runnable {
 		getContentPane().add(lblTitle);
 		
 		lblAutoPin = new JLabel("<html>Please enter your auto generated pin this is your date of birth and the four digits provided e.g. ddmmyy0000</html>");
-		lblAutoPin.setBounds(173, 89, 343, 66);
+		lblAutoPin.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblAutoPin.setBounds(173, 89, 330, 66);
 		getContentPane().add(lblAutoPin);
 		
 		txtAutoPin = new JTextField();
@@ -32,11 +45,29 @@ public class Login extends JFrame implements Runnable {
 		getContentPane().add(txtAutoPin);
 		txtAutoPin.setColumns(10);
 		
+		file = new File(path);
+		try {
+
+			image = ImageIO.read(file);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		
+		lblimg = new JLabel(new ImageIcon(image));
+		lblimg.setBounds(10, 170, 154, 129);
+		getContentPane().add(lblimg);
+		
 	}
 
 	@Override
 	public void run() {
 
-		
+		setSize(520, 300);
+		setResizable(false);
+		setTitle("Your Solution");
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
 	}
 }
