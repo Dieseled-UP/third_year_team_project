894ea9b69f8e1771b963250e5385aa7650e90729
denis bourne <bourne.denis@gmail.com>
2015-02-19 22:16:22 +0000

    label and text area for auto pin added

--- team_nerd_bank/src/gui/Login.java
+++ team_nerd_bank/src/gui/Login.java
@@ -3,20 +3,34 @@ package gui;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JTextField;
 
 
 public class Login extends JFrame implements Runnable {
 
 	private static final long serialVersionUID = 1L;
+	private JTextField txtAutoPin;
+	private JLabel lblAutoPin;
+	private JLabel lblTitle;
 	
 	public Login() {
 		
 		getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("NERD BANK");
-		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 31));
-		lblNewLabel.setBounds(219, 12, 214, 49);
-		getContentPane().add(lblNewLabel);
+		lblTitle = new JLabel("NERD BANK");
+		lblTitle.setFont(new Font("Dialog", Font.BOLD, 31));
+		lblTitle.setBounds(219, 12, 214, 49);
+		getContentPane().add(lblTitle);
+		
+		lblAutoPin = new JLabel("<html>Please enter your auto generated pin this is your date of birth and the four digits provided e.g. ddmmyy0000</html>");
+		lblAutoPin.setBounds(173, 89, 343, 66);
+		getContentPane().add(lblAutoPin);
+		
+		txtAutoPin = new JTextField();
+		txtAutoPin.setFont(new Font("Dialog", Font.PLAIN, 14));
+		txtAutoPin.setBounds(305, 136, 175, 21);
+		getContentPane().add(txtAutoPin);
+		txtAutoPin.setColumns(10);
 		
 	}
 
