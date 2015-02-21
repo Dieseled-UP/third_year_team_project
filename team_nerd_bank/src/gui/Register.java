71b16646226c8ad0a820c52f6223b5e0ee2b5149
denis <bourne.denis@gmail.com>
2015-02-21 23:50:05 +0000

    starting form layout for registration class

--- team_nerd_bank/src/gui/Register.java
+++ team_nerd_bank/src/gui/Register.java
@@ -1,16 +1,65 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JTextField;
 
 
 public class Register extends JFrame implements Runnable {
+	public Register() {
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(93, 11, 357, 53);
+		getContentPane().add(lblNewLabel);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(93, 93, 357, 358);
+		getContentPane().add(panel);
+		panel.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblFName = new JLabel("New label");
+		panel.add(lblFName, "2, 4");
+		
+		txtFName = new JTextField();
+		panel.add(txtFName, "14, 4, fill, default");
+		txtFName.setColumns(10);
+	}
 
 	private static final long serialVersionUID = 1L;
-	private int test;
+	private JTextField txtFName;
+	
+	
+	
 	@Override
 	public void run() {
 
 		
 	}
-
 }
