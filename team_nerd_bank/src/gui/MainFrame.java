497bb72d3be12456efbefd051e9e341b00d705a3
denis bourne <bourne.denis@gmail.com>
2015-02-26 16:24:36 +0000

    cardlayout added to main

--- team_nerd_bank/src/gui/MainFrame.java
+++ team_nerd_bank/src/gui/MainFrame.java
@@ -7,6 +7,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
+import java.awt.CardLayout;
 
 
 /**
@@ -20,7 +21,9 @@ public class MainFrame extends JFrame implements Runnable {
 	private JButton btnAccounts;
 	private JButton btnTransfers;
 	private JButton btnDetails;
+	private JButton btnPayees;
 	private JButton btnLogout;
+	private JPanel panel_1;
 	
 	
 	public MainFrame() {
@@ -29,28 +32,38 @@ public class MainFrame extends JFrame implements Runnable {
 		
 		panel = new JPanel();
 		panel.setBackground(Color.CYAN);
-		panel.setBounds(10, 11, 824, 69);
+		panel.setBounds(10, 12, 824, 69);
 		getContentPane().add(panel);
 		
 		btnAccounts = new JButton("Accounts");
 		btnAccounts.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		btnAccounts.setBounds(10, 79, 151, 42);
+		btnAccounts.setBounds(10, 81, 151, 42);
 		getContentPane().add(btnAccounts);
 		
 		btnTransfers = new JButton("Transfers");
 		btnTransfers.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		btnTransfers.setBounds(10, 119, 151, 42);
+		btnTransfers.setBounds(10, 121, 151, 42);
 		getContentPane().add(btnTransfers);
 		
 		btnDetails = new JButton("Details");
 		btnDetails.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		btnDetails.setBounds(10, 159, 151, 42);
+		btnDetails.setBounds(10, 161, 151, 42);
 		getContentPane().add(btnDetails);
+		
+		btnPayees = new JButton("Payees");
+		btnPayees.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		btnPayees.setBounds(10, 201, 151, 42);
+		getContentPane().add(btnPayees);
 		
 		btnLogout = new JButton("Logout");
 		btnLogout.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		btnLogout.setBounds(10, 199, 151, 42);
+		btnLogout.setBounds(10, 242, 151, 42);
 		getContentPane().add(btnLogout);
+		
+		panel_1 = new JPanel();
+		panel_1.setBounds(173, 93, 663, 445);
+		getContentPane().add(panel_1);
+		panel_1.setLayout(new CardLayout(0, 0));
 	}
 
 
