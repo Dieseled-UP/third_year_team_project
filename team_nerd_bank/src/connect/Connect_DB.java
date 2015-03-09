8b7213f71049abf7fdc3631dd57da1dd268af5bb
denis <bourne.denis@gmail.com>
2015-03-09 15:58:32 +0000

    access to data based changed

--- team_nerd_bank/src/connect/Connect_DB.java
+++ team_nerd_bank/src/connect/Connect_DB.java
@@ -15,8 +15,8 @@ public class Connect_DB {
 	// declare variables
 	private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
 	private static final String DB_CONNECTION = "jdbc:mysql://52.16.53.121/the_bank";
-	private static final String DB_USER = "root";
-	private static final String DB_PASSWORD = "";
+	private static final String DB_USER = "admin";
+	private static final String DB_PASSWORD = "password";
 	private static Connection con = null;
 	private static PreparedStatement statement = null;
 
