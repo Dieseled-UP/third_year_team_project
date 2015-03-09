6ce8b2dec890ecba8ef26a9c9345316b82ddf4fc
AineH <l00101074@student.lyit.>
2015-03-09 18:25:28 +0000

    Created a pin panel and password panel with jlables and jtext boxes

--- team_nerd_bank/src/gui/UserPinPass.java
+++ team_nerd_bank/src/gui/UserPinPass.java
@@ -21,6 +21,7 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.swing.Icon;
+import javax.swing.JTextField;
 
 public class UserPinPass extends JFrame implements Runnable {
 
@@ -31,6 +32,10 @@ public class UserPinPass extends JFrame implements Runnable {
 	private File file;
 	private BufferedImage image;
 	private JPanel pnlHeader_1;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 
 	public UserPinPass() {
 		getContentPane().setLayout(null);
@@ -59,6 +64,60 @@ public class UserPinPass extends JFrame implements Runnable {
 		JLabel lblLogo = new JLabel(new ImageIcon(image));
 		lblLogo.setBounds(10, -1, 70, 70);
 		pnlHeader_1.add(lblLogo);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(100, 116, 396, 88);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblEnterPin = new JLabel("Enter Pin");
+		lblEnterPin.setForeground(Color.BLUE);
+		lblEnterPin.setBounds(0, 14, 67, 14);
+		panel.add(lblEnterPin);
+		
+		JLabel lblRenterPin = new JLabel("Renter Pin");
+		lblRenterPin.setForeground(Color.BLUE);
+		lblRenterPin.setBounds(0, 45, 67, 14);
+		panel.add(lblRenterPin);
+		
+		textField = new JTextField();
+		textField.setBounds(154, 11, 86, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(154, 42, 86, 20);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(100, 215, 396, 88);
+		getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setBounds(198, 5, 0, 0);
+		panel_1.add(label_1);
+		
+		JLabel lblNewLabel = new JLabel("Enter Password");
+		lblNewLabel.setForeground(Color.BLUE);
+		lblNewLabel.setBounds(0, 22, 120, 14);
+		panel_1.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Renter Password");
+		lblNewLabel_1.setForeground(Color.BLUE);
+		lblNewLabel_1.setBounds(0, 47, 120, 14);
+		panel_1.add(lblNewLabel_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(149, 19, 86, 20);
+		panel_1.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(149, 44, 86, 20);
+		panel_1.add(textField_3);
+		textField_3.setColumns(10);
 
 	}
 
