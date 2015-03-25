1b74eaa8148669b2def4d60e1d9399a0087bc545
Lasek <Lasek@Lasek-PC>
2015-03-25 13:24:05 +0000

    AES_2 - working test Displays clear-text and encrypted pin/pass

--- team_nerd_bank/src/security/Test.java
+++ team_nerd_bank/src/security/Test.java
@@ -5,15 +5,28 @@ team_nerd_bank
 
 package security;
 
+import java.util.ArrayList;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Template;
+import com.sun.org.apache.xml.internal.security.encryption.EncryptedData;
+
+import connect.Query;
+
 public class Test {
     
     public static void main(String[] args) throws Exception {
  
-        AES d = new AES();
-             
-        System.out.println("Encrypted string:" + d.encrypt("Hello"));          
-        String encryptedText = d.encrypt("Hello");
-        System.out.println("Decrypted string:" + d.decrypt(encryptedText));      
- 
+        
+      Query test = new Query();
+      
+      test.getLogin(703809876);
+      
+      ArrayList<String> pinPass = test.getLogin(703809876);
+      
+      String pinString = new String(pinPass.get(0));
+      String pasString = new String(pinPass.get(1));
+      
+      AES encAes = new AES();
+      encAes.encryptPinPass(pinString, pasString);
     }
 }
