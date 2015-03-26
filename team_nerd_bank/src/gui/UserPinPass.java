642381f85537b4a940849abdef52b0132bc3f18a
Dieseled-UP <bourne.denis@gmail.com>
2015-03-26 13:52:39 +0000

    verfication added to the pin and password fields in UserPinPass to ensure user enter correct details

--- team_nerd_bank/src/gui/UserPinPass.java
+++ team_nerd_bank/src/gui/UserPinPass.java
@@ -5,43 +5,43 @@
  */
 package gui;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.UIManager;
-import javax.swing.WindowConstants;
-
 import java.awt.Color;
 import java.awt.Cursor;
-
-import javax.swing.JLabel;
-
 import java.awt.Font;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
-import javax.swing.Icon;
-import javax.swing.JTextField;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.InputVerifier;
 import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.WindowConstants;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
 public class UserPinPass extends JFrame implements Runnable {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel pnlHeader;
 	private JLabel lblLogo;
 	private String path = "Assets/logo2.svg.png";
 	private File file;
 	private BufferedImage image;
-	private JPanel pnlHeader_1;
-	private JTextField txtPin;
-	private JTextField txtRePin;
-	private JTextField txtPassword;
-	private JTextField txtRePassword;
+	private JPanel pnlHeader;
+	private JPasswordField txtPin;
+	private JPasswordField txtRePin;
+	private JPasswordField txtPassword;
+	private JPasswordField txtRePassword;
 	private JPanel panel;
 	private JPanel panel_1;
 	private JLabel lblEnterPin;
@@ -51,10 +51,11 @@ public class UserPinPass extends JFrame implements Runnable {
 	private JLabel lblRePassword;
 	private JLabel label;
 	private JPanel panel_2;
+	private JLabel lblPassword;
 
 	public UserPinPass() {
 		getContentPane().setBackground(Color.WHITE);
-		
+
 		try {
 			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
 				if ("Nimbus".equals(info.getName())) {
@@ -66,20 +67,20 @@ public class UserPinPass extends JFrame implements Runnable {
 			// If Nimbus is not available, you can set the GUI to another look
 			// and feel.
 		}
-		
+
 		getContentPane().setLayout(null);
 
-		pnlHeader_1 = new JPanel();
-		pnlHeader_1.setLayout(null);
-		pnlHeader_1.setBackground(new Color(0, 0, 139));
-		pnlHeader_1.setBounds(10, 11, 637, 69);
-		getContentPane().add(pnlHeader_1);
+		pnlHeader = new JPanel();
+		pnlHeader.setLayout(null);
+		pnlHeader.setBackground(new Color(0, 0, 139));
+		pnlHeader.setBounds(10, 11, 637, 69);
+		getContentPane().add(pnlHeader);
 
 		label = new JLabel("PMDA BANK");
 		label.setForeground(Color.WHITE);
 		label.setFont(new Font("Serif", Font.BOLD, 40));
 		label.setBounds(124, 11, 259, 48);
-		pnlHeader_1.add(label);
+		pnlHeader.add(label);
 
 		file = new File(path);
 		try {
@@ -92,89 +93,165 @@ public class UserPinPass extends JFrame implements Runnable {
 
 		lblLogo = new JLabel(new ImageIcon(image));
 		lblLogo.setBounds(10, -1, 70, 70);
-		pnlHeader_1.add(lblLogo);
-		
+		pnlHeader.add(lblLogo);
+
 		panel = new JPanel();
-		panel.setBorder(new TitledBorder(new LineBorder(new Color(255, 165, 0), 1, true), "Create Pin", TitledBorder.LEADING, TitledBorder.TOP, new Font("Tahoma", Font.BOLD, 14), Color.BLUE));
+		panel.setBorder(new TitledBorder(new LineBorder(new Color(255, 165, 0),
+				1, true), "Create Pin", TitledBorder.LEADING, TitledBorder.TOP,
+				new Font("Tahoma", Font.BOLD, 14), Color.BLUE));
 		panel.setBounds(100, 116, 456, 88);
 		getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		lblEnterPin = new JLabel("Enter Pin");
 		lblEnterPin.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblEnterPin.setForeground(Color.BLUE);
 		lblEnterPin.setBounds(117, 18, 67, 14);
 		panel.add(lblEnterPin);
-		
+
 		lblRenterPin = new JLabel("Renter Pin");
 		lblRenterPin.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblRenterPin.setForeground(Color.BLUE);
-		lblRenterPin.setBounds(117, 43, 67, 14);
+		lblRenterPin.setBounds(117, 55, 67, 14);
 		panel.add(lblRenterPin);
-		
-		txtPin = new JTextField();
-		txtPin.setBounds(211, 17, 86, 20);
+
+		txtPin = new JPasswordField();
+		txtPin.setBounds(211, 14, 86, 26);
 		panel.add(txtPin);
 		txtPin.setColumns(16);
-		
-		txtRePin = new JTextField();
-		txtRePin.setBounds(211, 42, 86, 20);
+
+		txtRePin = new JPasswordField();
+		txtRePin.setBounds(211, 51, 86, 26);
+		txtRePin.setInputVerifier(new verificationPin());
 		panel.add(txtRePin);
 		txtRePin.setColumns(16);
-		
+
 		panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(new LineBorder(new Color(255, 165, 0), 1, true), "Create Password", TitledBorder.LEADING, TitledBorder.TOP, new Font("Tahoma", Font.BOLD, 14), Color.BLUE));
+		panel_1.setBorder(new TitledBorder(new LineBorder(
+				new Color(255, 165, 0), 1, true), "Create Password",
+				TitledBorder.LEADING, TitledBorder.TOP, new Font("Tahoma",
+						Font.BOLD, 14), Color.BLUE));
 		panel_1.setBounds(100, 215, 456, 88);
 		getContentPane().add(panel_1);
 		panel_1.setLayout(null);
-		
-		JLabel label_1 = new JLabel("");
-		label_1.setBounds(198, 5, 0, 0);
-		panel_1.add(label_1);
-		
-		JLabel lblPassword = new JLabel("Enter Password");
+
+		lblPassword = new JLabel("Enter Password");
 		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblPassword.setForeground(Color.BLUE);
 		lblPassword.setBounds(78, 22, 120, 14);
 		panel_1.add(lblPassword);
-		
+
 		lblRePassword = new JLabel("Renter Password");
 		lblRePassword.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblRePassword.setForeground(Color.BLUE);
-		lblRePassword.setBounds(78, 47, 120, 14);
+		lblRePassword.setBounds(78, 55, 120, 14);
 		panel_1.add(lblRePassword);
-		
-		txtPassword = new JTextField();
-		txtPassword.setBounds(218, 21, 86, 20);
+
+		txtPassword = new JPasswordField();
+		txtPassword.setBounds(218, 18, 160, 26);
 		panel_1.add(txtPassword);
 		txtPassword.setColumns(16);
-		
-		txtRePassword = new JTextField();
-		txtRePassword.setBounds(218, 46, 86, 20);
+
+		txtRePassword = new JPasswordField();
+		txtRePassword.setBounds(218, 51, 160, 26);
+		txtRePassword.setInputVerifier(new verificationPass());
 		panel_1.add(txtRePassword);
 		txtRePassword.setColumns(10);
-		
+
 		panel_2 = new JPanel();
 		panel_2.setBounds(139, 314, 395, 50);
 		getContentPane().add(panel_2);
 		panel_2.setLayout(null);
-		
+
 		btnOK = new JButton("OK");
 		btnOK.setBackground(new Color(201, 216, 239));
 		btnOK.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnOK.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		btnOK.setBounds(59, 11, 89, 23);
+		btnOK.setBounds(59, 11, 89, 26);
 		panel_2.add(btnOK);
-		
+
 		btnCancel = new JButton("Cancel");
 		btnCancel.setBackground(new Color(201, 216, 239));
 		btnCancel.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnCancel.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		btnCancel.setBounds(221, 11, 89, 23);
+		btnCancel.setBounds(221, 11, 89, 26);
 		panel_2.add(btnCancel);
-		
-		
 
+		btnOK.addActionListener(arg0 -> {
+
+			// Check that the user has filled out all fields
+			if (txtRePin.getPassword() == null
+					&& txtRePassword.getPassword() == null) {
+
+				JOptionPane.showMessageDialog(null,
+						"Please fill out all sections before proceeding");
+			} else {
+
+				java.awt.EventQueue.invokeLater(() -> {
+
+					MainFrame frame = new MainFrame();
+					SwingUtilities.invokeLater(frame);
+
+				});
+			}
+		});
+
+		btnCancel.addActionListener(arg0 -> {
+
+			dispose();
+		});
+
+	}
+
+	/**
+	 * Check that the to pin numbers entered match
+	 */
+	class verificationPin extends InputVerifier {
+
+		@Override
+		public boolean verify(JComponent input) {
+
+			JPasswordField tField = (JPasswordField) input;
+
+			if (Arrays.equals(tField.getPassword(), txtPin.getPassword())) {
+
+				return true;
+
+			} else {
+
+				// Instruct user that the pin numbers don't match
+				JOptionPane.showMessageDialog(null,
+						"Sorry the fields don't match");
+
+				txtRePin.setText(null);
+				tField.setText(null);
+				return false;
+			}
+		}
+	}
+
+	class verificationPass extends InputVerifier {
+
+		@Override
+		public boolean verify(JComponent input) {
+
+			JPasswordField tField = (JPasswordField) input;
+
+			if (Arrays.equals(tField.getPassword(), txtPassword.getPassword())) {
+
+				return true;
+
+			} else {
+
+				// Instruct user that the pin numbers don't match
+				JOptionPane.showMessageDialog(null,
+						"Sorry the fields don't match");
+
+				txtRePassword.setText(null);
+				tField.setText(null);
+				return false;
+			}
+		}
 	}
 
 	@Override
